987bb8d4ba05ba14e4145ddaa7223cf461618645
ChrisUscanga <christianuscangaparra@gmail.com>
2025-05-30 16:32:31 -0600

    front end inicial

---
+++ EstresReloj/app/src/main/java/com/example/estresreloj/presentation/MainActivity.kt
@@ -0,0 +1,93 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
+ * most up to date changes to the libraries and their usages.
+ */
+
+package com.example.estresreloj.presentation
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import androidx.wear.tooling.preview.devices.WearDevices
+import com.example.estresreloj.R
+import com.example.estresreloj.presentation.theme.EstresRelojTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        setTheme(android.R.style.Theme_DeviceDefault)
+
+        setContent {
+            WearApp("Android")
+        }
+    }
+}
+
+@Composable
+fun WearApp(greetingName: String) {
+    EstresRelojTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            TimeText()
+            Greeting(greetingName = greetingName)
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    val context = LocalContext.current;
+    Column (horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.primary,
+            text = stringResource(R.string.prop_def, greetingName)
+        )
+        Button(
+            onClick = {
+                Toast.makeText(context, "Bienvenido", Toast.LENGTH_LONG).show()
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Default.PlayArrow,
+                contentDescription = "Run"
+            )
+        }
+    }
+}
+//_____________________________________
+
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp("Preview Android")
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RelojCodex-9A
  El RelojCodex-9A es un sistema avanzado de monitoreo agrícola que utiliza drones equipados con sensores para detectar plagas en cultivos. Este sistema cuenta con un reloj inteligente que recibe alertas en tiempo real y envía notificaciones push a los usuarios, permitiendo un seguimiento preciso del proceso de detección y control de plagas.
+
+
+# NOTA 
+el nombre del proyecto se llama estrés Reloj pero esto es porque se reutilizo el proyecto de clase se uso el mismo y como se cambio el tema del proyectó por eso el nombre no concuerda 
